ffdd4b189667ed66e6c1d8ddd9d39e0d86449613
Erik J Brown <erkjbro@gmail.com>
2021-01-20 16:46:20 -0600

    fetching api message

--- web-client/src/App.js
+++ web-client/src/App.js
@@ -1,9 +1,35 @@
+import { useEffect, useState } from 'react';
+
 const App = () => {
+  const [response, setResponse] = useState();
+
+  useEffect(() => {
+    (async () => {
+      try {
+        // Loading True
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}`);
+        const responseData = await response.json();
+
+        if (!response.ok) {
+          throw new Error(responseData.message);
+        }
+
+        // Loading False
+        setResponse(responseData);
+      } catch (err) {
+        console.error(err);
+      }
+    })();
+  }, []);
+
   return (
     <div>
       <h1>Verbose Octo Blog</h1>
+      {response && (
+        <p>{response.message}</p>
+      )}
     </div>
   );
-};
+}
 
 export default App;
