22607c984ac143e50e600f3941b0bf80ab863db9
Erik J Brown <erkjbro@gmail.com>
2021-01-23 18:16:37 -0600

    module conversion for top level await

--- node-api/index.js
+++ node-api/index.js
@@ -1,6 +1,6 @@
-const bodyParser = require('body-parser');
-const express = require('express');
-const mongoose = require('mongoose');
+import bodyParser from 'body-parser';
+import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
