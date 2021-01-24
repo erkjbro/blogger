ae93cb6d719d5bb2c0c0bb85dad17338622f56a9
Erik J Brown <erkjbro@gmail.com>
2021-01-23 18:23:54 -0600

    added some route handling

--- node-api/index.js
+++ node-api/index.js
@@ -38,6 +38,15 @@ app.get('/api', (req, res, next) => {
   res.json({ message: 'blog api reached!'});
 });
 
+app.use((req, res, next) => {
+  res.status(404).json({ message: 'Sorry, I don\'t have that...' });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something broke!' });
+});
+
 const PORT = Number(process.env.PORT) || 5000;
 app.listen(PORT, () => {
   console.log(`Listening on ${PORT}...`)
