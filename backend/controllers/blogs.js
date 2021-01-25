3d17e5aa07c692e072f36065231dcdbcafb2e338
Erik J Brown <erkjbro@gmail.com>
2021-01-25 14:46:39 -0600

    adding dummy data

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,6 +1,20 @@
 
+let DUMMY_BLOGS = [
+  {
+    title: 'A Test Blog',
+    content: 'Lorem Ipsum goes here...',
+    author: 'u1'
+  },
+  {
+    title: 'My First Blog!',
+    content: 'Hey, this is my first blog post since signing up!',
+    author: 'u2'
+  }
+];
+
 export const getBlogs = (req, res, next) => {
   res.status(200).json({
-    message: 'Fetched blog posts!'
+    message: 'Fetched blogs!',
+    blogs: DUMMY_BLOGS
   });
 };
--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,6 +1,22 @@
 
+let DUMMY_USERS = [
+  {
+    id: 'u1',
+    name: 'Erik Brown',
+    email: 'erik@example.com',
+    password: 'password'
+  },
+  {
+    id: 'u2',
+    name: 'Super User',
+    email: 'user@example.com',
+    password: 'supersecret'
+  }
+];
+
 export const getUsers = (req, res, next) => {
   res.status(200).json({
-    message: "Fetched Users!"
+    message: "Fetched Users!",
+    users: DUMMY_USERS
   });
 };
