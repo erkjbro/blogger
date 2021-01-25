6d9942b4f171ea3e1dedade1dc6e4079f78555ca
Erik J Brown <erkjbro@gmail.com>
2021-01-25 15:08:18 -0600

    fetching dummy data in client

--- backend/controllers/blogs.js
+++ backend/controllers/blogs.js
@@ -1,13 +1,15 @@
 
 let DUMMY_BLOGS = [
   {
+    id: 'b1',
     title: 'A Test Blog',
-    content: 'Lorem Ipsum goes here...',
+    content: 'Hey there! My name is Erik. I am a full-stack web developer that is passionate about the MERN stack. ',
     author: 'u1'
   },
   {
+    id: 'b2',
     title: 'My First Blog!',
-    content: 'Hey, this is my first blog post since signing up!',
+    content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
     author: 'u2'
   }
 ];
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,26 @@
 import { useEffect, useState } from 'react';
 
 const App = () => {
-  const [response, setResponse] = useState();
+  const [blogs, setBlogs] = useState();
+  const [users, setUsers] = useState();
 
   useEffect(() => {
     (async () => {
       try {
         // Loading True
-        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}`);
+        const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/blogs`);
         const responseData = await response.json();
 
-        if (!response.ok) {
+        const userRes = await fetch(`${process.env.REACT_APP_BACKEND_URL}/users`);
+        const userResData = await userRes.json();
+
+        if (!response.ok && !userRes.ok) {
           throw new Error(responseData.message);
         }
 
         // Loading False
-        setResponse(responseData);
+        setBlogs(responseData);
+        setUsers(userResData);
       } catch (err) {
         console.error(err);
       }
@@ -25,7 +30,35 @@ const App = () => {
   return (
     <div>
       <h1>Verbose Octo Blog</h1>
-      <p>Node API message: <code>{response ? response.message : 'null'}</code></p>
+      <hr />
+
+      <h2>BLOGS</h2>
+
+      <p>Node API blogs message: <code>{blogs ? blogs.message : 'null'}</code></p>
+
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {blogs && blogs.blogs.map((b) => (
+          <li key={b.id} style={{ border: '2px solid white', margin: '1rem', padding: '1rem 4rem' }}>
+            <h3>{b.title}</h3>
+            <p>{b.content}</p>
+          </li>
+        ))}
+      </ul>
+
+      <hr />
+
+      <h2>USERS</h2>
+
+      <p>Node API users message: <code>{users ? users.message : 'null'}</code></p>
+
+      <ul style={{ listStyle: 'none', padding: 0 }}>
+        {users && users.users.map((u) => (
+          <li key={u.id} style={{ textAlign: 'left', width: '300px', margin: 'auto' }}>
+            <p><span>User's Name: {u.name}</span></p>
+            <p><span>User's Email: {u.email}</span></p>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
