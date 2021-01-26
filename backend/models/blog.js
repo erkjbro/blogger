15c386876faf06b2ebd2b1af4b2af471fad98fcb
Erik J Brown <erkjbro@gmail.com>
2021-01-26 16:02:45 -0600

    created data basic models

---
+++ backend/models/blog.js
@@ -0,0 +1,23 @@
+import mongoose from 'mongoose';
+
+const { Schema, model } = mongoose;
+
+const blogSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  content: {
+    type: String,
+    required: true
+  },
+  creator: {
+    type: mongoose.Types.ObjectId,
+    ref: 'User',
+    required: true
+  }
+}, {
+  timestamps
+});
+
+export default model('Blog', blogSchema);
---
+++ backend/models/user.js
@@ -0,0 +1,30 @@
+import mongoose from 'mongoose';
+import uniqueValidator from 'mongoose-unique-validator';
+
+const { Schema, model } = mongoose;
+
+const userSchema = Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true,
+    minLength: 6
+  },
+  blogs: [{
+    type: mongoose.Types.ObjectId,
+    ref: 'Blog',
+    required: true
+  }]
+});
+
+userSchema.plugin(uniqueValidator)
+
+export default model('User', userSchema);
