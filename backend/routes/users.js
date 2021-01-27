ea40d4e380bac699c9a03f6ea60f0231fb237c1c
Erik J Brown <erkjbro@gmail.com>
2021-01-26 23:08:14 -0600

    adding user route validation

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,9 +1,44 @@
 import express from 'express';
+import { check } from 'express-validator';
 
 import * as userController from '../controllers/users.js';
 
 const router = express.Router();
 
-router.get('/', userController.getUsers);
+router.get(
+  '/',
+  userController.getUsers
+);
+
+router.post(
+  '/signup',
+  [
+    check('name')
+      .not()
+      .isEmpty(),
+
+    check('email')
+      .normalizeEmail()
+      .isEmail(),
+
+    check('password')
+      .isLength({ min: 6 })
+  ],
+  userController.postSignup
+);
+
+router.post(
+  '/login',
+  [
+    check('email')
+      .normalizeEmail()
+      .isEmail(),
+
+    check('password')
+      .not()
+      .isEmpty()
+  ],
+  userController.postLogin
+);
 
 export default router;
