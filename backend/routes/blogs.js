c88f9b4631ff40b99b6e1c018c08601f6278a5f6
Erik J Brown <erkjbro@gmail.com>
2021-03-16 11:12:25 -0500

    wiring blog routes & validation checks

--- backend/routes/blogs.js
+++ backend/routes/blogs.js
@@ -1,9 +1,50 @@
 import express from 'express';
+import { check } from 'express-validator';
 
 import * as blogController from '../controllers/blogs.js';
+// need to add a middleware for auth checks
 
 const router = express.Router();
 
-router.get('/', blogController.getBlogs);
+router.get(
+  '/',
+  blogController.getBlogs
+);
+
+router.get(
+  '/:blogId',
+  blogController.getBlogById
+);
+
+router.post(
+  '/',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+
+    check('content')
+      .isLength({ min: 5 })
+  ],
+  blogController.postBlog
+);
+
+router.patch(
+  '/:blogId',
+  [
+    check('title')
+      .not()
+      .isEmpty(),
+
+    check('content')
+      .isLength({ min: 5 })
+  ],
+  blogController.patchBlog
+);
+
+router.delete(
+  '/:blogId',
+  blogController.deleteBlog
+);
 
 export default router;
