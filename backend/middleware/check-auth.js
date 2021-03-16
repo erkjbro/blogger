8abab50c052926bcf3ce6c7fc499517ec602323b
Erik J Brown <erkjbro@gmail.com>
2021-03-16 16:31:26 -0500

    changing how a missing uid is handled

--- backend/middleware/check-auth.js
+++ backend/middleware/check-auth.js
@@ -12,12 +12,20 @@ const checkAuth = (req, res, next) => {
     // 3) Decode token by verifying with jwt key
     // 4) Extract userId from decoded token
 
-    console.log(process.env.DUMMY_USER_ID);
+    const userId = process.env.DUMMY_USER_ID;
+
+    if (!userId) {
+      const error = new HttpError(
+        'Authentication failed!',
+        403
+      );
+      return next(error);
+    }
 
     // To keep testing simple, a userId is being stored in the env.
     // This will be refined later when jwt's are introduced.
     req.userData = {
-      userId: process.env.DUMMY_USER_ID || 'u1'
+      userId: process.env.DUMMY_USER_ID
     };
 
     next();
