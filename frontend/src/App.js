1666b10eed2d44f53d9d5b696cb0c84960605b74
Erik J Brown <erkjbro@gmail.com>
2021-03-25 10:26:39 -0500

    routing update with lazy loading

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,49 @@
-import { useContext } from 'react';
+import { useContext, lazy, Suspense } from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 import Home from './blog/pages/Home';
 import { AuthContext } from './shared/context/AuthContext';
 
+const Auth = lazy(() => import('./user/pages/Auth'));
+const Profile = lazy(() => import('./user/pages/Profile'));
+
 const App = () => {
   const context = useContext(AuthContext);
 
+  let routes;
   if (!!context.token) {
-    console.log("Protected routes rendered!");
+    routes = (
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/:userId/profile" component={Profile} />
+        <Redirect to="/" />
+      </Switch>
+    );
+  } else {
+    routes = (
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/auth" exact component={Auth} />
+        <Redirect to="/" />
+      </Switch>
+    );
   }
 
+  const loading = (
+    <div>
+      <h2>Loading...</h2>
+    </div>
+  );
+
   return (
-    <Switch>
-      <Route path="/" exact component={Home} />
-      <Redirect to="/" />
-    </Switch>
+    <>
+      <nav>Nav...</nav>
+      <main>
+        <Suspense fallback={loading}>
+          {routes}
+        </Suspense>
+      </main>
+    </>
   );
 };
 
