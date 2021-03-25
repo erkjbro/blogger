cfa027a556931cfa8377adf370b3e8d4e6e8fee6
Erik J Brown <erkjbro@gmail.com>
2021-03-25 10:26:28 -0500

    placeholders for auth & profile pages

---
+++ frontend/src/user/pages/Auth.js
@@ -0,0 +1,8 @@
+
+const Auth = () => (
+  <div>
+    <h2>Login &amp; Signup...</h2>
+  </div>
+);
+
+export default Auth;
---
+++ frontend/src/user/pages/Profile.js
@@ -0,0 +1,8 @@
+
+const Profile = (props) => (
+  <div>
+    <h2>User profile page.</h2>
+  </div>
+);
+
+export default Profile;
