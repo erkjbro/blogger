6ec527a445f0d7399560c65509bf9cc7bb39b33e
Erik J Brown <erkjbro@gmail.com>
2021-03-27 17:09:35 -0500

    building & styling auth forms

--- frontend/src/user/pages/Auth.js
+++ frontend/src/user/pages/Auth.js
@@ -1,12 +1,97 @@
-import { useEffect } from 'react';
+// eslint-disable-next-line
+import { useState, useEffect, useContext } from 'react';
+
+// import ErrorMessage from '../../shared/components/UIKit/ErrorMessage/ErrorMessage';
+// import Loader from '../../shared/components/UIKit/Loader/Loader';
+// import useFetch from '../../shared/hooks/useFetch';
+// import { AuthContext } from '../../shared/context/AuthContext';
+import './Auth.scss';
 
 const Auth = () => {
+  // const { login, signup } = useContext(AuthContext);
+  const [isLoginMode, setIsLoginMode] = useState(true);
+  const [form, setForm] = useState({
+    name: {
+      value: ''
+    },
+    email: {
+      value: ''
+    },
+    password: {
+      value: ''
+    }
+  })
+
   useEffect(() => document.title = "Authentication | VOB", []);
 
+  const handleAuthSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(form);
+  };
+
+  const handleAuthToggle = () => setIsLoginMode(prevState => !prevState);
+
   return (
-    <div>
-      <h2>Authentication</h2>
-    </div>
+    <>
+      <div className="auth">
+        <h2>Authentication</h2>
+        <form onSubmit={handleAuthSubmit}>
+          <h4>{isLoginMode ? "LOGIN" : "SIGNUP"}</h4>
+          {!isLoginMode && (
+            <label>
+              Name
+              <input
+                id="name"
+                type="text"
+                placeholder="Name"
+                value={form.name.value}
+                onChange={(event) => setForm({
+                  ...form,
+                  name: {
+                    value: event.target.value
+                  }
+                })}
+              />
+            </label>
+          )}
+          <label>
+            E-Mail
+            <input
+              id="email"
+              type="email"
+              placeholder="E-Mail"
+              value={form.email.value}
+              onChange={(event) => setForm({
+                ...form,
+                email: {
+                  value: event.target.value
+                }
+              })}
+            />
+          </label>
+          <label>
+            Password
+            <input
+              id="password"
+              type="password"
+              placeholder="Password"
+              value={form.password.value}
+              onChange={(event) => setForm({
+                ...form,
+                password: {
+                  value: event.target.value
+                }
+              })}
+            />
+          </label>
+          <button type="submit">{!isLoginMode ? "SIGNUP" : "LOGIN"}</button>
+        </form>
+        <button className="toggle" onClick={handleAuthToggle}>
+          SWITCH TO {isLoginMode ? "SIGNUP" : "LOGIN"}
+        </button>
+      </div>
+    </>
   );
 };
 
