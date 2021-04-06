387f2aab48939d758b5d8d84a84b3a0fa0e3e480
Erik J Brown <erkjbro@gmail.com>
2021-04-06 16:28:51 -0500

    creating a uikit card

---
+++ frontend/src/shared/components/UIKit/Card/Card.js
@@ -0,0 +1,31 @@
+import clsx from 'clsx';
+
+import './Card.scss';
+
+const Card = (props) => {
+  const {
+    className,
+    styles,
+    children,
+    ...rest
+  } = props;
+
+  const classes = clsx(
+    {
+      uikit__card: true
+    },
+    className
+  );
+
+  return (
+    <div
+      className={classes}
+      styles={styles}
+      {...rest}
+    >
+      {children}
+    </div>
+  );
+};
+
+export default Card;
