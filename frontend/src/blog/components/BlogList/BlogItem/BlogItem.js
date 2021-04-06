78ad8c72dfe959eb154a9892dec80a15a70b9521
Erik J Brown <erkjbro@gmail.com>
2021-04-06 16:35:19 -0500

    updating em approach for exports of uikit card

--- frontend/src/blog/components/BlogList/BlogItem/BlogItem.js
+++ frontend/src/blog/components/BlogList/BlogItem/BlogItem.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-import Card from '../../../../shared/components/UIKit/Card/Card';
+import { Card } from '../../../../shared/components/UIKit';
 import './BlogItem.scss';
 
 const BlogItem = (props) => {
---
+++ frontend/src/shared/components/UIKit/index.js
@@ -0,0 +1,11 @@
+export {
+  Card
+} from './Card/Card';
+
+// export {
+//   ErrorMessage
+// } from './ErrorMessage/ErrorMessage';
+
+// export {
+//   Loader
+// } from './Loader/Loader';
