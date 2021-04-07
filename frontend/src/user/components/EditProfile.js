b79ac036629bcaacb6b7c8ac7dc4ab478b562575
Erik J Brown <erkjbro@gmail.com>
2021-04-07 11:54:26 -0500

    utilizing uikit card for editing profile

--- frontend/src/user/components/EditProfile.js
+++ frontend/src/user/components/EditProfile.js
@@ -1,5 +1,6 @@
 import { Link } from 'react-router-dom';
 
+import { Card } from '../../shared/components/UIKit';
 import './EditProfile.scss';
 
 const EditProfile = (props) => {
@@ -9,31 +10,33 @@ const EditProfile = (props) => {
     <div className="edit__profile">
       <h1>Edit Profile</h1>
       <Link to={url}>View Profile</Link>
-      <form onSubmit={onUserEdit}>
-        <label>
-          Name:
+      <Card className="edit__profile--card">
+        <form className="edit__profile--form" onSubmit={onUserEdit}>
+          <label>
+            Name:
           <input
-            value={user.name.value}
-            onChange={event => setUser({
-              ...user,
-              name: {
-                value: event.target.value
-              }
-            })}
-          />
-        </label>
-        <label>
-          Email:
+              value={user.name.value}
+              onChange={event => setUser({
+                ...user,
+                name: {
+                  value: event.target.value
+                }
+              })}
+            />
+          </label>
+          <label>
+            Email:
           <input
-            className="primary__email"
-            value={user.email.value}
-            onChange={() => { console.warn("Primary email can't be changed.") }}
-          />
-        </label>
-        <button type="submit">
-          Save Changes
+              className="primary__email"
+              value={user.email.value}
+              onChange={() => { console.warn("Primary email can't be changed.") }}
+            />
+          </label>
+          <button type="submit">
+            Save Changes
         </button>
-      </form>
+        </form>
+      </Card>
       <span>
         <button style={{ color: 'red' }} onClick={onUserDelete}>Delete Your Account</button>
       </span>
